63949bec2d22d084e65a1cdeed88673fbbe7bfa0
GiauNGUYEN12 <giaunguyen0216@gmail.com>
2025-01-08 14:40:51 +0100

    Ajouter Champ saissie l'heure travail et l'heure pause

--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
-const workSessionDuration = 10
-const breakDuration = 5
-let time = 5;
+const workSessionDuration = document.querySelector('#workTime')
+const breakDuration = document.querySelector('#breakTime')
+const usersInput = document.querySelector("#usersInput")
+let time = breakDuration.value;
 let timeToWork = true;
 let workInterval;
 const timerElement = document.querySelector("#timer");
-const buttonStart = document.querySelector(".button");
+const buttonStart = document.querySelector("#buttonStart");
 const buttonEnd = document.querySelector("#buttonEnd")
 
 const decreaseTime = () => {
@@ -21,22 +22,43 @@ const decreaseTime = () => {
 
   if (time < 0) {
     timeToWork = !timeToWork; // Alterner entre travail et pause
-    time = timeToWork ? workSessionDuration : breakDuration; // Repasser à 10 secondes ou 5 secondes
+    time = timeToWork ? workSessionDuration.value : breakDuration.value; // Repasser à 10 secondes ou 5 secondes
   }
 }
 
 const startTimer = () => {
     clearInterval(workInterval);
     timeToWork = true;
-    time = workSessionDuration;
+    time = workSessionDuration.value;
     decreaseTime();
     workInterval = setInterval(decreaseTime, 1000);
 };
 
 // Démarrer automatiquement au chargement
-buttonStart.addEventListener("click", startTimer);
+buttonStart.addEventListener("click", () =>{
+    startTimer()
+    buttonEnd.style.display = "block"
+    buttonStart.style.display = "none"
+    usersInput.innerHTML= ""
+})
 
-buttonEnd.addEventListener('click', () => {
+buttonEnd.addEventListener('click', endTimer)
+
+function endTimer (){
     clearInterval(workInterval)
     timerElement.innerText = ``;
-})
+    buttonEnd.style.display = "none"
+    buttonStart.style.display = "block"
+    usersInput.innerHTML = `
+    <label for="workTime">Choisissez l'heure travail (m)</label>
+    <input 
+        type="number" 
+        id="workTime"
+        min="0">
+    <label for="breakTime">Choisissez l'heure pause (m)</label>
+    <input 
+        type="number" 
+        id="breakTime"
+        min="0">
+`
+}
