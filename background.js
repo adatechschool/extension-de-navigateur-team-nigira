78d5c9bc2d0240abf6d5fa1f25f27e297257f6d9
AssiarDev <halibaraissa@gmail.com>
2025-01-15 16:11:01 +0100

    update

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.offscreen.createDocument({
-  url: 'popup.html',
-  reasons: [chrome.offscreen.Reason.GEOLOCATION || chrome.offscreen.Reason.DOM_SCRAPING],
-  justification: 'geolocation access',
-});
