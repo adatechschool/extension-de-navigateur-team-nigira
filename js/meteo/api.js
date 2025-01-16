808cb3aa0a9a76cee5420f9b93b8ae7963e6fb4b
nicordo19 <poiraudnico@gmail.com>
2025-01-16 11:01:44 +0100

    photo

--- js/meteo/api.js
+++ js/meteo/api.js
@@ -1,13 +1,13 @@
-const apiKey = "cd093ab592052b071cdee63c68dd7189";
+const apiKey = "a47e2ddf470395a5b0404198ae1e51e0";
 let result = document.querySelector("#result");
 
 export const inputCity = document.querySelector("#inputcity");
 
-export async function tempExterieur(city) { 
+export async function tempExterieur(city) {
   const meteoApi = await fetch(
     `https://api.weatherstack.com/current?access_key=${apiKey}&query=${city}`
   );
-  
+
   const data = await meteoApi.json();
   result.textContent = `A ${data.location.name} il fait ${data.current.temperature}°C,`;
 }
